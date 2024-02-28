99607e0b583d9807464bf149e8672e29c0de0cfe
jugiraldoces <juandiego.giraldocespedes@mercadolibre.com.co>
2024-02-28 10:07:34 -0500

    control de excepciones por serializacion y validaciones

--- src/main/java/org/bootcamp/javazoo/dto/ProductDto.java
+++ src/main/java/org/bootcamp/javazoo/dto/ProductDto.java
@@ -17,26 +17,26 @@ public class ProductDto {
 
     @NotEmpty (message = "The 'name' cannot be empty.")
     @Size (max = 40, message = "The 'name' cannot exceed 40 characters.")
-    @Pattern(regexp = "(^[a-zA-Z0-9 ]*$)", message = "The 'name' cannot have special characters (%, &, $, etc.)")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "The 'name' cannot have special characters (%, &, $, etc.)")
     private String product_name;
 
     @NotEmpty (message = "The 'type' cannot be empty.")
     @Size (max = 15, message = "The 'type' cannot exceed 15 characters.")
-    @Pattern(regexp = "(^[a-zA-Z0-9]*$)", message = "The 'type' cannot have special characters (%, &, $, etc.) or spaces")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The 'type' cannot have special characters (%, &, $, etc.) or spaces")
     private String type;
 
     @NotEmpty (message = "The 'brand' cannot be empty.")
     @Size (max = 25, message = "The 'brand' cannot exceed 25 characters.")
-    @Pattern(regexp = "(^[a-zA-Z0-9]*$)", message = "The 'brand' cannot have special characters (%, &, $, etc.) or spaces")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "The 'brand' cannot have special characters (%, &, $, etc.) or spaces")
     private String brand;
 
     @NotEmpty (message = "The 'color' cannot be empty.")
     @Size (max = 15, message = "The 'color' cannot exceed 15 characters.")
-    @Pattern(regexp = "(^[a-zA-Z0-9]*$)", message = "The 'color' cannot have special characters (%, &, $, etc.) or spaces")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "The 'color' cannot have special characters (%, &, $, etc.) or spaces")
     private String color;
 
     @Size (max = 80, message = "The 'notes' cannot exceed 80 characters.")
-    @Pattern(regexp = "(^[a-zA-Z0-9 ]*$)", message = "The 'notes' cannot have special characters (%, &, $, etc.)")
+    @Pattern(regexp = "^[a-zA-Z0-9 ]+$", message = "The 'notes' cannot have special characters (%, &, $, etc.)")
     private String notes;
 
 }
